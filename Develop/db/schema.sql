b592fd27cc43f1375fc906a6d0d80fd5b2768f96
MadStoddard <madelinejdierking@gmail.com>
2022-05-23 21:30:04 -0400

    added routes

--- Develop/db/schema.sql
+++ Develop/db/schema.sql
@@ -4,3 +4,25 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- CREATE TABLE Category (
+--     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--     category_name VARCHAR(30) NOT NULL
+-- );
+
+-- CREATE TABLE Product (
+--     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--     product_name VARCHAR(30) NOT NULL,
+--     salary DECIMAL NOT NULL,
+--     category_id INTEGER 
+-- );
+
+-- CREATE TABLE Tag (
+--     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--     tag_name VARCHAR(30),
+-- );
+
+-- CREATE TABLE ProductTag (
+--     id INTEGER PRIMARY KEY AUTO_INCREMENT NOT NULL,
+--     product_id INTEGER,
+--     tag_id INTEGER
+-- );
